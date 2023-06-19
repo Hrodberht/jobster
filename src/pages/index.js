c712c4cc8ddef89d0ec8a51634f84f8bb778bd14
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-06-19 09:13:23 +0100

    Add setup pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "./Landing";
+import Error from "./Error";
+import Landing from "./Landing";
+import Register from "./Register";
+
+export { Landing, Error, Dashboard, Register };
