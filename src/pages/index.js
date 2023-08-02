6843127e0b4d602fa9a99ec8e7e38e331dd78519
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-08-02 09:10:40 +0100

    Add dashboard setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import Dashboard from "./Landing";
 import Error from "./Error";
 import Landing from "./Landing";
 import Register from "./Register";
 
-export { Landing, Error, Dashboard, Register };
+export { Landing, Error, Register };
