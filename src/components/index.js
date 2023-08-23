108b09a3e315de7c9ddfe75fd8bb30d440aaddef
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-08-23 09:18:02 +0100

    Add Job and Search Containers structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,19 @@
 import BigSidebar from "./BigSidebar";
 import FormRow from "./FormRow";
 import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
+import SearchContainer from "./SearchContainer";
 import SmallSidebar from "./SmallSidebar";
 
-export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar, FormRowSelect };
+export {
+  Logo,
+  FormRow,
+  BigSidebar,
+  Navbar,
+  SmallSidebar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+};
